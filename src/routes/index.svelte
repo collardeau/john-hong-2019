f191880827a26e1754decf2a4588721c6369da88
Thomas Collardeau <thomas@collardeau.com>
2019-10-15 12:38:05 +0100

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="bg-red-500">Great success!</h1>
+
+<figure>
+  <img alt="Borat" src="great-success.png" />
+  <figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
