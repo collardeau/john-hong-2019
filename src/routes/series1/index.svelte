733da2416cd4bb9b1a4cd9929a361d41af32bda6
Thomas Collardeau <thomas@collardeau.com>
2019-10-17 18:43:04 +0100

    create a feed comp

--- src/routes/series1/index.svelte
+++ src/routes/series1/index.svelte
@@ -9,23 +9,15 @@
 </script>
 
 <script>
+  import Feed from "../../components/Feed.svelte";
   export let posts;
 </script>
-
-<style>
-  ul {
-    margin: 0 0 1em 0;
-    line-height: 1.5;
-  }
-</style>
 
 <svelte:head>
   <title>Series 1</title>
 </svelte:head>
 
-<ul>
-  {#each posts as post}
-    <h1>{post.title}</h1>
-    <img src={post.img} alt={post.title} />
-  {/each}
-</ul>
+<section class="">
+  <h3 class="text-white p-4">Mixed Media series</h3>
+  <Feed {posts} />
+</section>
