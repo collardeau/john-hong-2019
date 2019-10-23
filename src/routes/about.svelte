2cdb5acc917716fd177307f7b5f95346ccc74452
Thomas Collardeau <thomas@collardeau.com>
2019-10-23 16:54:08 +0100

    add about info

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,54 @@
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<section class="bg-gray-100 p-4">
+  <h3 class="font-semibold">Artist Statement</h3>
+  <p>
+    My work is like a song. You can feel what it says before you know what it
+    means. I am influenced by Korean temple painting and the Japanese Rinpa
+    school. The use of handmade paper in much of my work helps to build a bridge
+    between drawing and painting. Some of my inspirations are improvisational
+    music, mythology and dance.
+  </p>
+  <h3 class="font-semibold mt-4">About John Hong</h3>
+  <p>
+    John Hong is an artist who currently lives and works in Providence, Rhode
+    Island. His work can be found in private collections in New York, Los
+    Angeles, San Francisco, Boston, Philadelphia, Seattle, Washington D.C., San
+    Diego, Oakland, Nashville, London, Seoul, Hong Kong, and Shanghai.
+  </p>
+  <h3 class="font-semibold mt-4">Achievements</h3>
+  <ul>
+    <li class="mt-2">
+      <b>2005</b>
+      <span>
+        M.F.A. Visual Art
+        <br />
+        The New School — Parsons School of Design
+        <br />
+        New York City
+        <br />
+      </span>
+    </li>
+    <li class="mt-2">
+      <b>2002</b>
+      <span>
+        Dana Pond Award Recipient
+        <br />
+        School of the Museum of Fine Arts
+        <br />
+        Boston
+      </span>
+    </li>
+    <li class="mt-2">
+      <b>1998</b>
+      <span>
+        B.A. Urban Studies & Art History
+        <br />
+        Boston University
+      </span>
+    </li>
+  </ul>
 
-<p>This is the 'about' page. There's not much here.</p>
+</section>
