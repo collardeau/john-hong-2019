163e1d7bb38cd0dfdb8668d293dcf1a922a89a3d
Thomas Collardeau <thomas@collardeau.com>
2019-10-26 14:48:05 +0100

    update cloudinary link

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const cloudinaryBase = ({ w, h } = { w: 0, h: 0 }) => {
   const width = w ? `w_${w},` : ""; // todo: round up?
   const height = h ? `h_${h},` : "";
-  return `https://res.cloudinary.com/dqnk3lc1i/image/fetch/${width}${height}f_auto/https://cranky-meninsky-867b19.netlify.com/`;
+  return `https://res.cloudinary.com/dqnk3lc1i/image/fetch/${width}${height}f_auto/https://john-hong-studio.com/`;
+  // return "";
 };
