f275a438f312b2cf83c9a69f0a02ebd6c1fdfbdd
Thomas Collardeau <thomas@collardeau.com>
2019-11-10 15:16:52 +0000

    roll up own lazy image loading

--- src/components/Feed.svelte
+++ src/components/Feed.svelte
@@ -1,46 +1,42 @@
 <script>
-  import { LazyLoadContainer, LazyLoad } from "svelte-lazyload";
   import FeedItem from "./FeedItem.svelte";
   import { makeCols } from "../utils/postsUtils";
   export let posts = [];
   export let title = "";
-
   $: cols = makeCols(posts, title);
 </script>
 
-<section class="max-w-5xl mx-auto mb-5 ">
-  <div class="mx-2 mx-0">
+<section class="mb-5">
+  <div class="mx-2 md:mx-0">
     <h3 class="py-4 mx-2 capitalize text-lg">series: {title}</h3>
-
     <div class="block md:hidden">
-      {#each posts.slice(0, 3) as item, i}
-        <div class:pt-4={i}>
+      {#each posts.slice(0, 2) as item, i}
+        <div class:mt-4={i}>
           <FeedItem {item} />
         </div>
       {/each}
-      <LazyLoadContainer>
-        {#each posts.slice(3) as item, i}
-          <LazyLoad id={item.slug}>
-            <div class="pt-4">
-              <FeedItem {item} />
-            </div>
-          </LazyLoad>
-        {/each}
-      </LazyLoadContainer>
+      {#each posts.slice(2) as item, i}
+        <div class="mt-4">
+          <FeedItem {item} lazy={true} />
+        </div>
+      {/each}
     </div>
 
-    <div class="hidden md:flex">
+    <div class="hidden md:flex max-w-5xl mx-auto">
       {#each cols as c}
         <div class="w-1/2 px-3">
-          {#each c as item, i}
-            <div class:pt-6={i}>
+          {#each c.slice(0, 3) as item, i}
+            <div class:mt-4={i}>
               <FeedItem {item} />
+            </div>
+          {/each}
+          {#each c.slice(3) as item}
+            <div class="mt-4">
+              <FeedItem {item} lazy={true} />
             </div>
           {/each}
         </div>
       {/each}
-
     </div>
-
   </div>
 </section>
--- src/components/FeedItem.svelte
+++ src/components/FeedItem.svelte
@@ -4,18 +4,35 @@
   import { outline } from "../theme";
 
   export let item = {};
+  export let lazy = false;
   const { img, imgW, imgH } = item;
-  const src = getHref(img, imgW);
+  const href = getHref(img, imgW);
 
+  let src = lazy && "IntersectionObserver" in window ? "" : getHref(img, imgW);
   let w;
   // give initial height (based on image size) for lazy loading
   $: h = !w ? 0 : Math.round((imgH / imgW) * w);
+
+  const onIntersect = entries => {
+    entries.forEach(e => {
+      if (!src && e.isIntersecting) src = href;
+    });
+  };
+
+  const observer = new IntersectionObserver(onIntersect, {
+    rootMargin: "500px"
+  });
+
+  function lazyImg(node) {
+    if (!lazy) return;
+    observer.observe(node);
+  }
 </script>
 
-<article bind:clientWidth={w}>
+<article bind:clientWidth={w} use:lazyImg>
   {#if h}
     <div style="height: {h}px;">
-      <img {src} alt={item.slug} />
+      <img {src} data-src={src} alt={item.slug} />
     </div>
   {/if}
   <section
